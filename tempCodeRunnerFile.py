c99bb9912703c848e7da45c5f51de2652d551187
Jacky V <71198906+jacklvd@users.noreply.github.com>
2022-05-24 01:31:16 -0400

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    # # foggy weather condition        
-    # elif int(current_time) < 20 and int(current_time) >= 6 and counter <= 10 and weather == 'Fog' or weather == 'Clear':
-    #     Frame(screen, width=800, height=350, bg='white').place(x=0, y=50)
-    #     img = ImageTk.PhotoImage(Image.open('assets/cold.png'))
-    #     Label(screen, image=img, border=0).place(x=170, y=130)
-    #     bcolor = 'white' # background color
-    #     fcolor = 'black' # font color
